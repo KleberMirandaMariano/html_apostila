fa9f5079eafd4803ef5475aa3b34531287c456f0
klebermd13 <62228769+klebermd13@users.noreply.github.com>
2020-06-23 22:42:15 -0300

    jQuery

---
+++ js/home.js
@@ -0,0 +1,13 @@
+$('.painel button').click(function(){
+    $(this).parent().toggleClass('painel-aberto');
+});
+
+/* uma função para cada item "display"
+$('.novidades button').click(function(){
+    $('.novidades').toggleClass('painel-aberto');
+});
+
+$('.mais-vendidos button').click(function(){
+    $('.mais-vendidos').toggleClass('painel-aberto');
+});
+*/
